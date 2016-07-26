08121442a68b322c38e778a00c192271cb8fa0d7
Mathew Brown <matt@mattbrown.com>
2016-07-26 16:49:22 -0600

    Complete

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,39 @@
 var app = angular.module('jwt',[]);
 
-app.controller('jwtController',['$scope', function($scope) {
-  $scope.view = {
-    greeting: "I'm Batman"
+app.config(function ($httpProvider) {
+  $httpProvider.interceptors.push('jwtInterceptor');
+})
+.service('jwtInterceptor', function jwtInterceptor(){
+  return {
+    // always make sure to return anything you use here!
+    request: function(config){
+      if (localStorage.jwt) {
+        config.headers.Authorization = 'Bearer ' + localStorage.jwt;
+      }
+      return config;
+    }
+  };
+})
+
+app.controller('jwtController',['$scope','$http', function($scope,$http) {
+
+  $scope.view = {};
+
+  $scope.anythingMan = function() {
+    $http.get('/login').then(function (res) {
+      localStorage.jwt = res.data.token;
+    });
+  };
+
+  $scope.useApi = function () {
+    $http.get('/api').then(function (res) {
+      $scope.view.response = res.data;
+    });
+  }
+
+  $scope.beerMe = function () {
+    $http.get('/ipa').then(function (res) {
+      $scope.view.response = res.data;
+    });
   }
 }]);
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 var express = require('express');
 var jwt = require('jsonwebtoken');
+var bearerToken = require('express-bearer-token');
 
 var app = express();
 
+app.use(bearerToken());
 app.use(express.static('public'));
 
 app.get('/login',function (req,res,next) {
@@ -13,6 +15,24 @@ app.get('/login',function (req,res,next) {
     isAdmin: false
   }
   res.json({token:jwt.sign(user,process.env.SECRET)});
+});
+
+app.use(function (req,res,next) {
+  jwt.verify(req.token, process.env.SECRET,function (err,decoded) {
+    if (!err) {
+      next();
+    } else {
+      res.status(400).send('Bad Request');
+    }
+  });
+});
+
+app.get('/api',function (req,res,next) {
+  res.send("Accessed Granted!");
+});
+
+app.get('/ipa',function (req,res,next) {
+  res.send("Sierra Nevada IPA for everyone on K C");
 });
 
 app.listen(3000);
