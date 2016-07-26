834be4190a25dfb367e92945a1480f635d39c028
Mathew Brown <matt@mattbrown.com>
2016-07-26 15:50:02 -0600

    Generated JWT

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
+require('dotenv').config();
 var express = require('express');
+var jwt = require('jsonwebtoken');
+
 var app = express();
 
+app.use(express.static('public'));
+
 app.get('/login',function (req,res,next) {
-  res.send('/login');
+  var user = {
+    name: "Nathan",
+    cohort: "g[23]",
+    isAdmin: false
+  }
+  res.json({token:jwt.sign(user,process.env.SECRET)});
 });
 
 app.listen(3000);
